e16b3b0051d435a4ab8d035c51ace3b104b4d3c5
Karan Jadhav <kj602011@gmail.com>
2025-04-13 00:55:40 +0530

    Added city service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const {PORT} = require('./config/serverSetup')
+const CityRepository = require('./reposotory/city-repository')
 
 const setupAndStartServer = ()=>{
   
@@ -10,6 +11,9 @@ const setupAndStartServer = ()=>{
     app.listen(PORT,()=>{
         console.log(`Server running at the PORT ${PORT}`);
         console.log(process.env.PORT);
+        const obj = new CityRepository();
+        // obj.createCity({name:"Tarale"});
+        // obj.deleteCity(1);
     })
 }
 
